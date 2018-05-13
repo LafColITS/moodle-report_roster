c466f624d907342f2adc283783f604f2516d71aa
Charles Fulton <fultonc@lafayette.edu>
2018-05-13 08:31:16 -0400

    Fix privacy API for Moodle 3.4+

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2018011000;
-$plugin->requires  = 2017051500;
+$plugin->requires  = 2017111300;
 $plugin->component = 'report_roster';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'v3.3.0';
